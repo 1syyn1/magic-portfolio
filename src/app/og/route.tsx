bf21b3d617777e049956f0264290d14bd1bf45df
Adan <adancetegamer@gmail.com>
2025-02-05 03:22:58 +0100

    updated img

--- src/app/og/route.tsx
+++ src/app/og/route.tsx
@@ -1,6 +1,5 @@
 import { ImageResponse } from "next/og";
 import { baseURL } from "@/app/resources";
-import { person } from "@/app/resources/content";
 
 export const runtime = "edge";
 
@@ -20,77 +19,23 @@ export async function GET(request: Request) {
         height: "100%",
         padding: "8rem",
         background: "#151515",
+        justifyContent: "center",
+        alignItems: "center",
+        fontFamily: "Inter",
+        color: "white",
       }}
     >
-      <div
+      <span
         style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          gap: "4rem",
-          fontFamily: "Inter",
-          fontStyle: "normal",
-          color: "white",
+          fontSize: "8rem",
+          lineHeight: "8rem",
+          letterSpacing: "-0.05em",
+          whiteSpace: "pre-wrap",
+          textWrap: "balance",
         }}
       >
-        <span
-          style={{
-            fontSize: "8rem",
-            lineHeight: "8rem",
-            letterSpacing: "-0.05em",
-            whiteSpace: "pre-wrap",
-            textWrap: "balance",
-          }}
-        >
-          {title}
-        </span>
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            gap: "5rem",
-          }}
-        >
-          <img
-            src={"https://" + baseURL + person.avatar}
-            style={{
-              width: "12rem",
-              height: "12rem",
-              objectFit: "cover",
-              borderRadius: "100%",
-            }}
-          />
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              gap: "0.75rem",
-            }}
-          >
-            <span
-              style={{
-                fontSize: "4.5rem",
-                lineHeight: "4.5rem",
-                whiteSpace: "pre-wrap",
-                textWrap: "balance",
-              }}
-            >
-              {person.name}
-            </span>
-            <span
-              style={{
-                fontSize: "2.5rem",
-                lineHeight: "2.5rem",
-                whiteSpace: "pre-wrap",
-                textWrap: "balance",
-                opacity: "0.6",
-              }}
-            >
-              {person.role}
-            </span>
-          </div>
-        </div>
-      </div>
+        {title}
+      </span>
     </div>,
     {
       width: 1920,
